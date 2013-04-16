4214c738bc348eeb44edce50da44f2d3f330f59b
Justin Miller <jmiller9@gmail.com>
2013-04-16 17:53:30 -0400

    Initial commit of JSF AJAX hw

---
+++ build/web/resources/js/util.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function show(id) {   
+            document.getElementById(id).style.display="inline";
+          };
+
+function hide(id) {   
+            document.getElementById(id).style.display="none";
+          };
